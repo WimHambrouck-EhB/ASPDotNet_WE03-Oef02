2cf9bfde6b6af5b4f7cfc11d7a08ff054a999a4c
Wim Hambrouck <wim.hambrouck@ehb.be>
2020-04-01 12:27:56 +0200

    Add project files.

---
+++ Werkcollege03.Oef02/Models/ToDo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Werkcollege03.Oef02.Models
+{
+    public class ToDo
+    {
+        public ToDo()
+        {
+            Datum = DateTime.Now;
+        }
+        public int ID { get; set; }
+        public string Titel { get; set; }
+        public DateTime Datum { get; private set; }
+        public string Omschrijving { get; set; }
+    }
+}
